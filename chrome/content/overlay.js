523749c59ffdb6c28307baae0cdfa9d91a9a4706
Toady <support@8pecxstudios.com>
2014-12-25 08:58:39 +1000

    Version 1.0.5

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -1,34 +1,17 @@
-// Copyright (c) 2014 8pecxstudios.com \\
-// use of restart my fox code is strictly by permissions only even when its very simple code. \\
+// Copyright (c) 2014 8pecxstudios.com 
 
+//Kris Maglione: (Quote) your code unduly difficult to read > > Fixed i hope its easier to read now sorry if the code was hard to read.
 var RestartMyFox = {
 	
 	restartBrowser: function() {	
 
 		try{
-			const Omega = Ci.nsIAppStartup;
-				var Alpha = Cc["@mozilla.org/observer-service;1"].getService(Ci.nsIObserverService);
-				var Zeus = Cc["@mozilla.org/supports-PRBool;1"].createInstance(Ci.nsISupportsPRBool);
-			Alpha.notifyObservers(Zeus, "quit-application-requested", null);
-   
-			if (Zeus.data)
-				return;
-				
-			Alpha.notifyObservers(null, "quit-application-granted", null);
-			
-				var pegasus = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
-				var Argos = pegasus.getEnumerator(null);
-				
-			while (Argos.hasMoreElements()) {
-				var MazdaRX7 = Argos.getNext();
-			if (("tryToClose" in MazdaRX7) && !MazdaRX7.tryToClose())
-				return;
-    }
-    Cc["@mozilla.org/toolkit/app-startup;1"].getService(Omega).quit(Omega.eRestart | Omega.eAttemptQuit);
+			//Kris Maglione: In any case, please just call `Application.restart` rather than implementing the logic yourself. >> I hope this was what you were after.
+			Cc["@mozilla.org/toolkit/app-startup;1"].getService(Ci.nsIAppStartup).quit(Ci.nsIAppStartup.eRestart | Ci.nsIAppStartup.eAttemptQuit);
 	
 			}catch (e){
-				//Catch any nasty errors and output to dialogue and console
-				alert("A Error Was Encountered While Attempting To Restart Browser! " + e);					
+				//Catch any nasty errors and output to dialogue message
+				alert("An error was encountered while attempting to restart browser! " + e);					
 		}
         
 	}	
