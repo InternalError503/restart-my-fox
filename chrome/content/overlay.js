f1e88fef136e83107deb647483b1f3b8e8a51277
Toady <support@8pecxstudios.com>
2014-12-25 08:59:30 +1000

    Version 1.0.6

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -1,12 +1,11 @@
 // Copyright (c) 2014 8pecxstudios.com 
 
-//Kris Maglione: (Quote) your code unduly difficult to read > > Fixed i hope its easier to read now sorry if the code was hard to read.
 var RestartMyFox = {
 	
 	restartBrowser: function() {	
 
 		try{
-			//Kris Maglione: In any case, please just call `Application.restart` rather than implementing the logic yourself. >> I hope this was what you were after.
+
 			Cc["@mozilla.org/toolkit/app-startup;1"].getService(Ci.nsIAppStartup).quit(Ci.nsIAppStartup.eRestart | Ci.nsIAppStartup.eAttemptQuit);
 	
 			}catch (e){
