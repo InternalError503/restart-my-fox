4d736a154057f5de7e781bbbd9d7bd5c17f3430c
Toady <support@8pecxstudios.com>
2015-02-28 10:06:30 +1000

    Add: MPL2.0 to head of files.

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -1,4 +1,6 @@
-// Copyright (c) 2014 8pecxstudios.com 
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 Cu.import("resource://gre/modules/Services.jsm");
 Services.prefs.QueryInterface(Ci.nsIPrefBranch);
