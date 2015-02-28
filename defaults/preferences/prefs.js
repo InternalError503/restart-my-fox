95bd54508351b8cc0c87cad5bd2fea563d95752e
Toady <support@8pecxstudios.com>
2015-02-28 14:55:16 +1000

    Revert "Added: Ability to set restart to require confirmation."
    This reverts commit e700b142b80f99a0673989d0b4c25e5f9b2e277e.

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,4 +1,3 @@
 pref("extensions.restart_my_fox.purgecache", false);
 pref("extensions.restart_my_fox.menubarbutton", true);
 pref("extensions.restart_my_fox.paneluibutton", false);
-pref("extensions.restart_my_fox.requireconfirm", false);
