e5065db1969bed4dbfb36891c72ffae5dec6c6d4
toady <support@8pecxstudios.com>
2015-03-10 22:06:17 +1000

    Put overlay.js though Javascript Beautifier to reformat.
    Here we reformatted to make it cleaner and easier to read.

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -1,80 +1,79 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
 Cu.import("resource://gre/modules/Services.jsm");
 Services.prefs.QueryInterface(Ci.nsIPrefBranch);
 
 var RestartMyFox = {
 
-	RMFBundle: Services.strings.createBundle("chrome://R-M-F/locale/RMFdialogue.properties"),
-	Branding: Services.strings.createBundle("chrome://branding/locale/brand.properties").GetStringFromName("brandShortName"),
-	
-	init: function (){
+    RMFBundle: Services.strings.createBundle("chrome://R-M-F/locale/RMFdialogue.properties"),
+    Branding: Services.strings.createBundle("chrome://branding/locale/brand.properties").GetStringFromName("brandShortName"),
 
-		try {
-				 document.getElementById("PanelUI-popup").addEventListener("popupshowing", function (e) {				 
-					if (!Services.prefs.getBoolPref("extensions.restart_my_fox.paneluibutton")){					
-						document.getElementById("panel_menu_948622EF9DD31D8EC28360C72957C429_restartBrowser").hidden = true;	
-					}else{
-						document.getElementById("panel_menu_948622EF9DD31D8EC28360C72957C429_restartBrowser").hidden = false;		
-					}
-					if(Services.prefs.getBoolPref("extensions.restart_my_fox.panelbtnsmall")){
-						document.getElementById("panel_menu_948622EF9DD31D8EC28360C72957C429_restartBrowser").className = "rmf_small_icon";
-					}else{
-						document.getElementById("panel_menu_948622EF9DD31D8EC28360C72957C429_restartBrowser").removeAttribute('class');
-					}
-				});
-				
-				 document.getElementById("toolbar-menubar").addEventListener("popupshowing", function (e) {					 
-					if (!Services.prefs.getBoolPref("extensions.restart_my_fox.menubarbutton")){					
-						document.getElementById("menu_948622EF9DD31D8EC28360C72957C429_restartBrowser").hidden = true;
-					}else{
-						document.getElementById("menu_948622EF9DD31D8EC28360C72957C429_restartBrowser").hidden = false;					
-					}		
-				});
+    init: function() {
 
-					window.addEventListener("beforecustomization", function (e) {
-						document.getElementById("panel_menu_948622EF9DD31D8EC28360C72957C429_restartBrowser").setAttribute('disabled', true);
-					}, false);
-					
-					window.addEventListener("aftercustomization", function (e) {
-						document.getElementById("panel_menu_948622EF9DD31D8EC28360C72957C429_restartBrowser").removeAttribute('disabled');
-					}, false);
-										
-		}catch (e){}	
-			
-	},
-	
-	restartBrowser: function() {	
+        try {
+            document.getElementById("PanelUI-popup").addEventListener("popupshowing", function(e) {
+                if (!Services.prefs.getBoolPref("extensions.restart_my_fox.paneluibutton")) {
+                    document.getElementById("panel_menu_948622EF9DD31D8EC28360C72957C429_restartBrowser").hidden = true;
+                } else {
+                    document.getElementById("panel_menu_948622EF9DD31D8EC28360C72957C429_restartBrowser").hidden = false;
+                }
+                if (Services.prefs.getBoolPref("extensions.restart_my_fox.panelbtnsmall")) {
+                    document.getElementById("panel_menu_948622EF9DD31D8EC28360C72957C429_restartBrowser").className = "rmf_small_icon";
+                } else {
+                    document.getElementById("panel_menu_948622EF9DD31D8EC28360C72957C429_restartBrowser").removeAttribute('class');
+                }
+            });
 
-		try{
-				
-			if (Services.prefs.getBoolPref("extensions.restart_my_fox.requireconfirm")){	
-				if (Services.prompt.confirm(null, this.RMFBundle.formatStringFromName("dialogue.title", [this.Branding], 1), 
-					this.RMFBundle.formatStringFromName("dialogue.message", [this.Branding], 1))){
-					
-						if (Services.prefs.getBoolPref("extensions.restart_my_fox.purgecache")){						
-							Services.appinfo.invalidateCachesOnRestart();
-							Services.startup.quit(Services.startup.eRestart | Services.startup.eAttemptQuit);							
-						}else{						
-							Services.startup.quit(Services.startup.eRestart | Services.startup.eAttemptQuit);
-						}
-				}
-			}else{
-				if (Services.prefs.getBoolPref("extensions.restart_my_fox.purgecache")){					
-					Services.appinfo.invalidateCachesOnRestart();
-					Services.startup.quit(Services.startup.eRestart | Services.startup.eAttemptQuit);						
-				}else{					
-					Services.startup.quit(Services.startup.eRestart | Services.startup.eAttemptQuit);
-				}
-			}
-			}catch (e){
-				//Catch any nasty errors and output to dialogue.
-				alert("Were sorry, Something has gone wrong while attempting to restart browser! " + e);					
-		}
-        
-	}	
-	
+            document.getElementById("toolbar-menubar").addEventListener("popupshowing", function(e) {
+                if (!Services.prefs.getBoolPref("extensions.restart_my_fox.menubarbutton")) {
+                    document.getElementById("menu_948622EF9DD31D8EC28360C72957C429_restartBrowser").hidden = true;
+                } else {
+                    document.getElementById("menu_948622EF9DD31D8EC28360C72957C429_restartBrowser").hidden = false;
+                }
+            });
+
+            window.addEventListener("beforecustomization", function(e) {
+                document.getElementById("panel_menu_948622EF9DD31D8EC28360C72957C429_restartBrowser").setAttribute('disabled', true);
+            }, false);
+
+            window.addEventListener("aftercustomization", function(e) {
+                document.getElementById("panel_menu_948622EF9DD31D8EC28360C72957C429_restartBrowser").removeAttribute('disabled');
+            }, false);
+
+        } catch (e) {}
+
+    },
+
+    restartBrowser: function() {
+
+        try {
+
+            if (Services.prefs.getBoolPref("extensions.restart_my_fox.requireconfirm")) {
+                if (Services.prompt.confirm(null, this.RMFBundle.formatStringFromName("dialogue.title", [this.Branding], 1),
+                        this.RMFBundle.formatStringFromName("dialogue.message", [this.Branding], 1))) {
+
+                    if (Services.prefs.getBoolPref("extensions.restart_my_fox.purgecache")) {
+                        Services.appinfo.invalidateCachesOnRestart();
+                        Services.startup.quit(Services.startup.eRestart | Services.startup.eAttemptQuit);
+                    } else {
+                        Services.startup.quit(Services.startup.eRestart | Services.startup.eAttemptQuit);
+                    }
+                }
+            } else {
+                if (Services.prefs.getBoolPref("extensions.restart_my_fox.purgecache")) {
+                    Services.appinfo.invalidateCachesOnRestart();
+                    Services.startup.quit(Services.startup.eRestart | Services.startup.eAttemptQuit);
+                } else {
+                    Services.startup.quit(Services.startup.eRestart | Services.startup.eAttemptQuit);
+                }
+            }
+        } catch (e) {
+            //Catch any nasty errors and output to dialogue.
+            alert("Were sorry, Something has gone wrong while attempting to restart browser! " + e);
+        }
+
+    }
+
 }
 RestartMyFox.init();
