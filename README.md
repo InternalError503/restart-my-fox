1fb3bc085f8f9ed3e007af58ea91fce1fba21e89
toady <support@8pecxstudios.com>
2016-07-25 13:23:35 +1000

    Add build support for vscode.
    Since vscode is cross platform we can use it to build restart my fox
    with python on any platform.

--- README.md
+++ README.md
@@ -7,11 +7,14 @@ Source Code Repository For Restart My Fox
 
 Source code released under [MPL 2.0] (https://www.mozilla.org/MPL/2.0/)
 
+
 #### What it does: 
+
 Adds to `appMenu (Legacy)` or `ToolsMenu` or `Tool-bar`, The Restart Browser menu item or Button that allows 
 users to easily restart the web browser without losing current open pages.
 
 ### About this Add-on:
+
 - Allows users to easily restart the web browser.
 - Keep all open pages.
 - Great if a plugin has stopped working and a restart is required to re-enable it.
@@ -20,4 +23,28 @@ users to easily restart the web browser without losing current open pages.
 
 Allows you to clear the browsers fast restart cache.
 
-Also available for Google chrome in [Chrome web store] (https://chrome.google.com/webstore/detail/restart-my-chrome/poifblphedfakpplnemomhgkhhpkpkla)
+
+###  To build (Platform):
+
+- `Windows:` __CTRL + SHIFT + B__
+- `Linux:` __CTRL + SHIFT + B__
+- `Mac:` __CMD + SHIFT + B__
+
+
+#### Task Runner (Visual Studio Code):
+
+- `Windows:` __CTRL + SHIFT + P__
+- `Linux:` __CTRL + SHIFT + P__
+- `Mac:` __CMD + SHIFT + P__
+
+| Task | Command | Result |
+|----------|:-------------:|------:|
+| Build | task build | Builds addon *.xpi |
+
+##### You must set the version number in the arguments field of tasks.json when bumping the XPI package version.
+
+
+#### Build Notes (Visual Studio Code) (Platform):
+
+- `Windows:` You must have __python 2.7__ or higher installed to run the build script.
+- `Linux:` You must on the __`build.py`__ set in its properties permissions tab, To allow execution or __`EACCESS`__ error will ensue.
---
+++ build.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+
+# Imports
+import os
+import re
+import sys
+
+from glob import glob
+from zipfile import ZipFile, ZIP_STORED, ZIP_DEFLATED
+
+# XPI package files, Note must update when adding or removing files.
+resources = [
+	"install.rdf",
+	"chrome.manifest",
+	"defaults/preferences/prefs.js",
+	"chrome/content/options.js",
+	"chrome/content/overlay.js",
+	"chrome/content/R-M-F.xul",
+	"chrome/content/RMF_options.xul",
+	"chrome/locale/*/*",
+	"chrome/skin/*"
+	]
+
+# Zip package
+class ZipOutFile(ZipFile):
+    def __init__(self, zfile):
+        ZipFile.__init__(self, zfile, "w", ZIP_DEFLATED)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.close()
+
+# Enumerate resource files and folders.	
+def get_files(resources):
+    for r in resources:
+        if os.path.isfile(r):
+            yield r
+            continue
+        for g in glob(r):
+            yield g
+
+# Build XPI package.
+def buildXPI( version ):
+	destination = "xpi/restart_my_fox-" + version + "-fx.xpi"
+	# Check if the package already exists in our destination and remove it.
+	if os.path.exists(destination):
+		print("Found & removing: " + destination)
+		os.remove(destination)
+	print('Creating package please wait!')	
+	with ZipOutFile(destination) as zp:
+		for f in sorted(get_files(resources), key=str.lower):
+			compress_type = ZIP_STORED if f.endswith(".png") else ZIP_DEFLATED
+			zp.write(f, compress_type=compress_type)
+			print("Compressing: " + f)
+	return;
+
+# Since we use command line arguments we require two in total "build" & "0.0.0" (Version) for XPI package.	
+if sys.argv[1] == "build":
+	# Make XPI directory if not exists.
+	if not os.path.exists("xpi"):
+		os.makedirs("xpi")
+	# Send version to build XPI package.
+	buildXPI(sys.argv[2])
+	sys.exit()
+else:
+	# If the above command line arguments were not set say goodbye.
+	print('Invalid commands or failed build!')
+	sys.exit()
